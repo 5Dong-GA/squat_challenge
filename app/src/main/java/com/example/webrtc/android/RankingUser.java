11fcfebc45c8f347e08fa1d7aad9b6ba18fee4f5
yunbamm <37677458+yunbamm@users.noreply.github.com>
2020-11-25 20:47:05 +0900

    랭킹 구현(ui 다듬어야함)

--- app/src/main/java/com/example/webrtc/android/RankingUser.java
+++ app/src/main/java/com/example/webrtc/android/RankingUser.java
@@ -3,11 +3,38 @@ package com.example.webrtc.android;
 public class RankingUser {
     String email;
     long total_count;
+    long speed_time;
 
     RankingUser(){}
 
-    RankingUser(String email , long total_count){
+    RankingUser(String email , long total_count , long speed_time){
         this.email = email;
         this.total_count= total_count;
+        this.speed_time = speed_time;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getTotal_count() {
+        return total_count;
+    }
+
+    public void setTotal_count(long total_count) {
+        this.total_count = total_count;
+    }
+
+    public long getSpeed_time() {
+        return speed_time;
+    }
+
+    public void setSpeed_time(long speed_time) {
+        this.speed_time = speed_time;
+    }
+
 }
